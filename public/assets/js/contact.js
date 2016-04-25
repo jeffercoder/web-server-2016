514a84035e3f55ea018e35d4ba8090bdeef6778b
jeffercoder <johnday68@gmail.com>
2016-04-25 07:46:53 -0500

    042416

--- public/assets/js/contact.js
+++ public/assets/js/contact.js
@@ -1,10 +1,10 @@
-submit = function() {
+ccontacts = function() {
   $('#name').val('');
   $('#replyto').val('');
   $('#msg').val('');
   // to jump back to top after clear
   $('html, body').animate({
-    scrollTop: ($('#contact-us').offset().top - 0)
+    scrollTop: ($('#').offset().top - 0)
   }, 1000);
   return false;
 // $('body').animate({scrollTop : 0}, 1000);
